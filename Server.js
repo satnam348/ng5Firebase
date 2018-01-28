5260931c6c18429bef114f348fce0d430c1085ab
Satnam Singh <Satnam.Singh156@T-Mobile.com>
2018-01-28 13:08:17 +0530

    update

--- Server.js
+++ Server.js
@@ -6,7 +6,11 @@ app.use(express.static(__dirname + '/dist'));
 app.get('*', function (req, res) {
   res.sendfile('./dist/index.html'); });
 
-port = process.env.PORT || 9000;
+//port = process.env.PORT || 3000;
 
-app.listen(port);
-console.log('todo list RESTful API server started on: ' + port);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
+// app.listen(port);
+// console.log('todo list RESTful API server started on: ' + port);
